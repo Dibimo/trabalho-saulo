937d7df399f2feac4006a22c08476c41c353128b
Dibimo <diogobissolimoreno@gmail.com>
2021-06-01 11:18:41 -0300

    Estilo e organização melhoradas

--- src/componentes/CardNota/index.js
+++ src/componentes/CardNota/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import estilo from './estilo'
 export default function CardNota({titulo, texto, categoria, acao}) {
     const [aberto, setAberto] = React.useState(false);
@@ -8,16 +9,39 @@ export default function CardNota({titulo, texto, categoria, acao}) {
         setAberto(!aberto);
     }
     return (
-        <View style={estilo.nota}>
-            <TouchableOpacity onPress={abreEFecha}><Text>{titulo} - {categoria}</Text></TouchableOpacity>
-            {aberto &&
-                (<View>
-                    <Text>{texto}</Text>
-                    <TouchableOpacity onPress={acao}>
-                        <Text>Deletar</Text>
-                    </TouchableOpacity>
-                </View>)
-            }
-        </View>        
+        <>
+            <View style={estilo.nota}>
+                <View style={{ display: "flex", flexDirection: "row",  }}>
+                    
+                    <View style={{ marginRight: 15, width: 225,}}>
+                        <TouchableOpacity onPress={abreEFecha}>
+                            <Text style={{ fontFamily: "flower"}}>{titulo} - {categoria}</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                    <View>
+                        <TouchableOpacity onPress={acao}>
+                            <Icon name="trash" size={18} color="#2eadff" />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+                {aberto &&
+                    (<View>
+                    <Text style={{ fontFamily: "flower" }}>{texto}</Text>
+                        {/* <TouchableOpacity onPress={acao}>
+                            <Text>Deletar</Text>
+                        </TouchableOpacity> */}
+                    </View>)
+                }
+            </View>
+
+            <View
+                style={{
+                    marginTop: 2,
+                    borderBottomColor: 'black',
+                    borderBottomWidth: 1,
+                }}
+            ></View>
+        </>
     );
 }
