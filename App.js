aff8eddfd2764d528aef056232a96c5e434a2943
Dibimo <diogobissolimoreno@gmail.com>
2021-06-01 11:15:42 -0300

    Fonte importada para o projeto

--- App.js
+++ App.js
@@ -3,8 +3,13 @@ import React from 'react';
 import Adicionar from './src/Telas/Adicionar'
 import Exibir from './src/Telas/Exibir'
 import Rotas from './src/Rotas';  
+import { useFonts } from 'expo-font';
 
 export default function App() {
+  
+  const [carregou] = useFonts({
+    flower: require('./assets/fonts/IndieFlower-Regular.ttf'),
+  });
   return (
     <Rotas />
   );
