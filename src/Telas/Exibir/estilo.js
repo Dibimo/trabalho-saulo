a3a23ea8e199b7a4be7d353ce4b3600f58dc17a0
Dibimo <diogobissolimoreno@gmail.com>
2021-06-01 11:19:24 -0300

    Estilo do body adicionado

--- src/Telas/Exibir/estilo.js
+++ src/Telas/Exibir/estilo.js
@@ -1,6 +1,10 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
+    bodyExibir: {
+        backgroundColor: "#e7deb7",
+        height: "100%"
+    },
     input: {
         height: '25%',
         margin: 12,
