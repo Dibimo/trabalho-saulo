4a0e92768f4a05e287df37cab95702c27cf540dc
Dibimo <diogobissolimoreno@gmail.com>
2021-06-01 11:17:17 -0300

    Estilização da página Adicionar

--- src/Telas/Adicionar/estilo.js
+++ src/Telas/Adicionar/estilo.js
@@ -1,12 +1,28 @@
 import { StyleSheet } from 'react-native';
+// import { useFonts } from '@expo-google-fonts/indie-flower';
+
 
 export default StyleSheet.create({
+    
+    littleLogoConteiner: {
+        display: "flex",
+        alignItems: "center",
+        marginBottom: -70,
+    },
+    littleLogo: {
+        display: "flex",
+        width: 225,
+        height: 225,
+        
+        resizeMode: 'contain'
+    },
     body:{
-        paddingTop: "30%",
+        paddingTop: "3%",
         backgroundColor: "#e7deb7",
         height: "100%"
     },
     input: {
+        fontFamily: 'flower',
         margin: 12,
     },
     listaDeNotas:{
