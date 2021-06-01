b4e29f721e37c729e76a1a869384e472c607667f
Dibimo <diogobissolimoreno@gmail.com>
2021-06-01 11:18:03 -0300

    Fonte customizada adicionada

--- src/componentes/CardNota/estilo.js
+++ src/componentes/CardNota/estilo.js
@@ -3,7 +3,9 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
     nota:{
 
-        borderWidth: 1,
+        // borderWidth: 1,
+        fontFamily: "flower",
+        borderBottomWidth: 1,
         borderColor: "#000",
         marginBottom: 2,
         width: '75%'
